027f2113c549492976f6b7bfd12c471fb0d0aae1
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-01 14:25:48 -0400

    Finished

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+public class App {
+
+    public static void main(String[] args) {
+        Student one = new Student("Jalen", 345, 4.3, true);
+        Student two = new Student("Jake", 222, 2.3, true);
+        Student three = new Student("Jason", 112, 1.3, false);
+        System.out.println("=== Student Profile manager ===");
+        System.out.println("Initial Student Profiles:");
+        one.getProfile(one);
+        three.isEnrolled();
+        two.getName();
+        two.getProfile(two);
+        three.getProfile(three);
+
+        System.out.println("=== Processing Student Changes ===");
+        three.enroll();
+        two.updateGpa(4.5);
+        one.withdraw();
+        one.updateGpa(5.0);
+        two.addCourse("Science");
+
+        System.out.println("Updated Student Profiles:");
+        one.getProfile(one);
+        two.getProfile(two);
+        three.getProfile(three);
+
+
+
+
+    }
+
+    public void Student(){
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Student.java
@@ -0,0 +1,82 @@
+package com.pluralsight;
+
+public class Student {
+    private String name;
+    private int studentId;
+    private double gpa;
+    private boolean isEnrolled;
+
+
+
+    public Student(String name, int studentId, double gpa, boolean isEnrolled) {
+        this.name = name;
+        this.studentId = studentId;
+        this.gpa = gpa;
+        this.isEnrolled = isEnrolled;
+    }
+    public Student() {
+        this.name = "";
+        this.studentId = 0;
+        this.gpa = 0.0;
+        this.isEnrolled = false;
+    }
+    public void enroll(){
+        System.out.println("Enrolling " + this.getName() + "...");
+        this.isEnrolled = true;
+
+    }
+    public void withdraw(){
+        System.out.println("Withdrawing " + this.getName() + "...");
+        this.isEnrolled = false;
+
+    }
+
+    public void updateGpa(double newgpa){
+        System.out.println("Updating " + this.name + "'s GPA to " + newgpa + "...");
+        this.gpa = newgpa;
+
+    }
+
+    public String addCourse(String test){
+        System.out.println("Adding " + test + " to " + this.name + "'s curriculum");
+        return test;
+
+    }
+
+    public void dropCourse(){
+
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public boolean isEnrolled() {
+        return isEnrolled;
+    }
+
+
+    public void getProfile(Student person){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(person.getName()).append(" (ID:");
+        sb.append(person.getStudentId()).append("): GPA: ");
+        sb.append(person.getGpa()).append(", Status: ");
+        sb.append(person.isEnrolled).append(System.lineSeparator());
+
+
+        String myInfo = sb.toString();
+
+        System.out.printf(myInfo);
+
+    }
+
+}
